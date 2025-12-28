49f4ce447f075dfc286bb6be787311bf2332a878
gabethemyers <gabethemyers@gmail.com>
2025-12-27 19:37:23 -0800

    update api base url and change sign in to use that url

--- frontend/app/signIn.tsx
+++ frontend/app/signIn.tsx
@@ -2,13 +2,14 @@ import { View, Text, Pressable, Linking } from 'react-native';
 import { useRouter } from 'expo-router';
 import { globalStyles } from '@/styles/globalStyle';
 import Header from '@/components/signInHeader';
-import { Stack } from 'expo-router'; 
+import { Stack } from 'expo-router';
+import { API_BASE_URL } from '@/utils/api';
 
 export default function SignInScreen() {
   const router = useRouter();
 
-  const githubAuthUrl = "https://group5project3-74e9cad2d6ba.herokuapp.com/oauth2/authorization/github";
-  const googleAuthUrl = "https://group5project3-74e9cad2d6ba.herokuapp.com/oauth2/authorization/google";
+  const githubAuthUrl = `${API_BASE_URL}/oauth2/authorization/github`;
+  const googleAuthUrl = `${API_BASE_URL}/oauth2/authorization/google`;
 
   return (
     <>
@@ -39,7 +40,7 @@ export default function SignInScreen() {
           <Text style={globalStyles.buttonText}>Sign in with Google</Text>
         </Pressable>
 
-        
+
 
         <Text style={{ marginTop: 12, color: '#666' }}>
           You can still browse cats without signing in.
